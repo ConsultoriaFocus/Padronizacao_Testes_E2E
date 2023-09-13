dbf456e8f61655bbdf7fe2484efb40c316d2d760
jg-moren <jgpmoren@gmail.com>
2023-09-13 12:00:01 -0300

    fix: toBeInTheDocument is not a function

--- src/test/Button.test.tsx
+++ src/test/Button.test.tsx
@@ -1,6 +1,6 @@
 import {render} from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect';
 import {Button} from '../components/Button'
-
 test("teste de compenente",()=>{
   const { getByText } = render(<Button/>)
   expect(getByText("test")).toBeInTheDocument()
